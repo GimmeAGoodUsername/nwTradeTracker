981b90173ae7226d4cc8190bf02984d1a54c7f23
Tim <timgustaw@gmail.com>
2021-04-07 11:56:26 +0200

    cleaned stuff

--- demo/src/main/java/com/example/demo/jpa/business/UserRepository.java
+++ demo/src/main/java/com/example/demo/jpa/business/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.jpa.business;
 import com.example.demo.jpa.model.UserModelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
